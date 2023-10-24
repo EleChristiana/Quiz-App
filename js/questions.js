13e3f5c6b34f6dc754265eb256c6f746c8dc6753
Calia21 <elechristacoal@gmail.com>
2023-10-23 20:53:34 -0700

    first commit

---
+++ js/questions.js
@@ -0,0 +1,124 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+
+    {
+     numb: 6,
+     question: "Which of the following keywords is used to define a variable in javascript",
+     answer: "let",
+      options: [  
+      "let",
+       "come",
+      "get",
+      "went"
+     ]
+   },
+
+   
+   {
+    numb: 7,
+    question: "What is the use of the noscript tag in javascript?",
+    answer: "The contents are displayed by non Js based browsers",
+     options: [  
+       "Clears all the cookies and cache",
+      "The contents are not displayed by all browsers",
+      "Only the value of the expression is compared",
+       "No accesss to coding JS"
+    ]
+  },
+
+  
+  {
+    numb: 8,
+    question: "What keyword is used to check whether a given property is valid or not?",
+    answer: "in",
+     options: [  
+     "in",
+      "as in",
+     "check",
+     "exists"
+    ]
+  },
+
+  
+  {
+    numb: 9,
+    question: "What does the javascript debugger statement do?",
+    answer: "It acts a breakpoint in a program",
+     options: [  
+     "it will debug all errors in the program at runtime",
+      "It will debug error in the current statement if any",
+     "All of the above",
+     "It acts a breakpoint in a program"
+    ]
+  },
+
+  
+  {
+    numb: 10,
+    question: "What does the LocateString() method in js",
+    answer: "Returns a localized string representation of an object",
+     options: [  
+     "Returns a localised object",
+      "Returns a localized string representation of an object",
+     "None of the above",
+     "Returns a parsed string"
+    ]
+  },
+
+]
